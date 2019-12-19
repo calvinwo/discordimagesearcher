f21c1ca53e59dfcf8d0d4e917c53857c00ac96e5
calvinwo <calvincdw@gmail.com>
2019-12-19 00:34:26 -1000

    add keep alive

--- index.js
+++ index.js
@@ -84,7 +84,33 @@ init();
 var http = require ('http');
 const PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
-
+  res.send('hello');
 }).listen(PORT, () => {
   console.log(`Our app is running on port ${ PORT }`);
 });
+
+startKeepAlive() 
+
+function startKeepAlive() {
+  setInterval(function() {
+      var options = {
+          host: 'https://powerful-caverns-88708.herokuapp.com/',
+          port: 80,
+          path: '/'
+      };
+      http.get(options, function(res) {
+          res.on('data', function(chunk) {
+              try {
+                  // optional logging... disable after it's working
+                  console.log("HEROKU RESPONSE: " + chunk);
+              } catch (err) {
+                  console.log(err.message);
+              }
+          });
+      }).on('error', function(err) {
+          console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
